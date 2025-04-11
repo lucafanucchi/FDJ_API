5a45e913dd38c03d19b43eac85b3dc27bf24460b
Luca <lucafanucchi@hotmail.com>
2025-04-11 02:00:44 -0300

    teste

--- spotify_utils.py
+++ spotify_utils.py
@@ -4,17 +4,17 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def get_spotify_tracks(playlist_url):
-    try:
-        SPOTIPY_CLIENT_ID = st.secrets["SPOTIPY_CLIENT_ID"]
-        SPOTIPY_CLIENT_SECRET = st.secrets["SPOTIPY_CLIENT_SECRET"]
+    #try:
+        #SPOTIPY_CLIENT_ID = st.secrets["SPOTIPY_CLIENT_ID"]
+        #SPOTIPY_CLIENT_SECRET = st.secrets["SPOTIPY_CLIENT_SECRET"]
 
-    except KeyError:
-        st.error("Credenciais do Spotify não configuradas corretamente no secrets.toml.")
-        return []
+    #except KeyError:
+        #st.error("Credenciais do Spotify não configuradas corretamente no secrets.toml.")
+        #return []
 
     auth_manager = SpotifyClientCredentials(
-        client_id=SPOTIPY_CLIENT_ID,
-        client_secret=SPOTIPY_CLIENT_SECRET
+        client_id="f33e019748a0487496fdb221c17361bd",
+        client_secret="1ae6295b04eb445caf071b3788a14d1c"
     )
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
